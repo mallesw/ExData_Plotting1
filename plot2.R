ce0596091fa927f30c56dc949d06e950e89f6d11
mallesw <92munkee@gmail.com>
2015-10-11 14:57:40 -0700

    add plot1.R, plot1.png, plot2.R, plot2.png, plot3.R, plot3.png, plot4.R, plot4.png

---
+++ plot2.R
@@ -0,0 +1,16 @@
+##read in file
+alldata <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                    nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+alldata$Date <- as.Date(alldata$Date, format="%d/%m/%Y")
+##subset data
+dat <- subset(alldata, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(alldata)
+
+datetime <- paste(as.Date(dat$Date), dat$Time)
+dat$datetime <- as.POSIXct(datetime)
+
+##plot2
+plot(dat$Global_active_power~dat$datetime, type="l",
+     ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,31 @@
+##read in file
+alldata <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                 nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+alldata$Date <- as.Date(alldata$Date, format="%d/%m/%Y")
+##subset data
+dat <- subset(alldata, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(alldata)
+
+datetime <- paste(as.Date(dat$Date), dat$Time)
+dat$datetime <- as.POSIXct(datetime)
+
+##plot 4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(dat, {
+        plot(Global_active_power~datetime, type="l", 
+             ylab="Global Active Power (kilowatts)", xlab="")
+        plot(Voltage~datetime, type="l", 
+             ylab="Voltage (volt)", xlab="datetime")
+        plot(Sub_metering_1~datetime, type="l", 
+             ylab="Global Active Power (kilowatts)", xlab="")
+        lines(Sub_metering_2~datetime,col='Red')
+        lines(Sub_metering_3~datetime,col='Blue')
+        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+               legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        plot(Global_reactive_power~datetime, type="l", 
+             ylab="Global Rective Power (kilowatts)",xlab="datetime")
+})
+
+## Saving to file
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
